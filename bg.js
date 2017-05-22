e9bb696e0d903b2a55ba87dab824732b97855876
Tyler Barron <tdawgg2@yahoo.com>
2017-05-22 14:06:14 -0700

    v1.3 - Attempt to stop instant-closing
    Made it save dTimer on every load, assuming the instant closing was
    because it hadn't saved the value in a significant amount of time

--- bg.js
+++ bg.js
@@ -21,6 +21,10 @@ function initialize_stuff() {
 	  if (isNaN(dTimer)){
 		  dTimer = 6500 //defaults to 6500 if NaN
 	  }
+	  chrome.storage.sync.set({'delayTimerSaved': dTimer}, function() {
+          // Notify that we saved.
+          console.log('dTimer Saved: ', dTimer);
+		  });
 	});
   return dTimer;
 };
